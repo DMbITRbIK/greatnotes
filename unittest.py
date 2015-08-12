24102922657c563e7f34ced28a319931c47f6a7a
DMbITRbIK <dmbitrbik29@gmal.com>
2015-08-12 21:33:57 +0300

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Dou(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://dou.ua/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_dou(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_link_text(u"Форум").click()
+        driver.find_element_by_link_text(u"Теми").click()
+        driver.find_element_by_link_text(u"Головна").click()
+        driver.find_element_by_link_text(u"Кар’єра в IT").click()
+        driver.find_element_by_link_text(u"Робота").click()
+        driver.find_element_by_link_text("Python Tech Lead").click()
+        driver.find_element_by_css_selector("ul.b-content-menu > li.sel").click()
+        driver.find_element_by_id("reply-btn-id").click()
+        driver.find_element_by_css_selector("div.login-button.btnLoginVk").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _blank | 30000]]
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
